c4fb4e5aff85003be9d9ca51818ed5f3ac847147
Sophia <65602967+Sophed@users.noreply.github.com>
2025-02-22 23:11:58 +0000

    add error log

--- log.go
+++ log.go
@@ -7,10 +7,11 @@ import (
 )
 
 const rset = "\033[0m"
-const dbug = "\033[35;1m"
-const info = "\033[34;1m"
+const dbug = "\033[34;1m"
+const info = "\033[32;1m"
 const warn = "\033[33;1m"
-const fatl = "\033[31;1m"
+const erro = "\033[31;1m"
+const fatl = "\033[35;1m"
 
 func timestamp() string {
 	return time.Now().Format("15:04")
@@ -26,12 +27,17 @@ func Info(msg interface{}) {
 	fmt.Fprintln(os.Stderr, timestamp(), info+"INFO"+rset, msg)
 }
 
-// Warn logs errors/warning messages
+// Warn logs warning messages
 func Warn(msg interface{}) {
 	fmt.Fprintln(os.Stderr, timestamp(), warn+"WARN"+rset, msg)
 }
 
-// Fatl logs an error message and calls os.Exit(1), exiting the program
+// Erro logs error messages
+func Erro(msg interface{}) {
+	fmt.Fprintln(os.Stderr, timestamp(), erro+"ERRO"+rset, msg)
+}
+
+// Fatl logs a message and calls os.Exit(1), exiting the program
 func Fatl(msg interface{}) {
 	fmt.Fprintln(os.Stderr, timestamp(), fatl+"FATL"+rset, msg)
 	os.Exit(1)
